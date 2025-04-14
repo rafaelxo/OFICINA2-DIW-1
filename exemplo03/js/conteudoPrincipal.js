7ff6c3cb5a9c45103986905a65fdcadae3fbb693
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-14 03:27:04 +0000

    Exemplo

--- exemplo03/js/conteudoPrincipal.js
+++ exemplo03/js/conteudoPrincipal.js
@@ -1,13 +1,31 @@
+// Adicionar estilos diretamente no módulo:
+function adicionarEstilos() {
+  const estilo = document.createElement('style');
+
+  estilo.textContent = `
+    /* Conteúdo principal */
+    .classe-conteudo-principal {
+        flex: 1; /* Faz com que o conteúdo principal ocupe todo o espaço restante disponível dentro do flexbox. */
+        padding: 1rem;
+        border: 3px solid orange;
+    }
+  `;
+
+  document.head.appendChild(estilo);
+}
+
 // Componente do conteúdo principal:
 export function gerarConteudoPrincipal() {
-    const conteudoPrincipal = document.getElementById('id-conteudo-principal');
+  adicionarEstilos();
 
-    conteudoPrincipal.innerHTML = `
-      <main class="classe-conteudo-principal">
-        <h2>Bem-vindo à Página Modular!</h2>
-        <p>Este é um exemplo de como dividir componentes em arquivos JavaScript usando import e export.</p>
-      </main>
-    `;
+  const conteudoPrincipal = document.getElementById('id-conteudo-principal');
+
+  conteudoPrincipal.innerHTML = `
+    <main class="classe-conteudo-principal">
+      <h2>Bem-vindo à Página Modular!</h2>
+      <p>Este é um exemplo de como dividir componentes em arquivos JavaScript usando import e export.</p>
+    </main>
+  `;
 }
 
 // Iniciar o componente:
